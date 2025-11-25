e42241d60e183173f34b251022731402275243f6
Isaac Bolivar Mendoza <89425523+ShakyLove@users.noreply.github.com>
2025-11-24 23:31:04 -0500

    Update environment.ts

--- pruebaFront/src/environments/environment.ts
+++ pruebaFront/src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API_URL: 'http://127.0.0.1:8000/api/',
+  production: true,
+  API_URL: 'https://proyecto-universidad-desarrollo-web.onrender.com/api/',
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
